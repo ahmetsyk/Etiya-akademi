001d4edd0a15c1e189d0157a4e75c1dcf7bb5a52
ahmetsyk <oslosahmet@gmail.com>
2023-09-23 17:44:58 +0300

    Canlı ders3 Class & Interface ile Sürdürülebilirlik

---
+++ java-2022/oopWithNLayeredApp/src/oopWithNLAyeredApp/core/logging/EmailLogger.java
@@ -0,0 +1,9 @@
+package oopWithNLAyeredApp.core.logging;
+
+public class EmailLogger implements Logger{
+
+    @Override
+    public void log(String message) {
+        System.out.println("Mail gönderildi: " + message);
+    }
+}
---
+++ java-2022/oopWithNLayeredApp/src/oopWithNLAyeredApp/dataAccess/ProductDao.java
@@ -0,0 +1,7 @@
+package oopWithNLAyeredApp.dataAccess;
+
+import oopWithNLAyeredApp.entities.Product;
+
+public interface ProductDao {
+    void add(Product product);
+}
